fe94c2965c1e1f00dc09546591da879453a0d076
saivenkatak33 <kolukuluriraju33@gmail.com>
2025-12-01 14:49:17 +0530

    done

--- true/src/components/video-call/index.js
+++
@@ -1,4 +0,0 @@
-// src/components/video_call/index.js
-export { default } from './VideoCall';
-export { default as CallControls } from './CallControls';
-export { default as CallModal } from './CallModal';
